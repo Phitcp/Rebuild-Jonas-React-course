240ef8b6949a2762884acec4c99c6ac0f53697cf
Phi <110226283+Phitcp@users.noreply.github.com>
2023-11-22 16:07:57 +0700

    Pizza menu

---
+++ 01-pizza-menu/src/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  return (
+    <>
+      <div className="footer">
+        <p>We're open from 12:00 to 22:00. Come visit us or order online</p>
+      </div>
+      <p className="text-button">Order</p>
+    </>
+  );
+}
+
+export default Footer;
